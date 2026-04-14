38dc620db2a7e2a50c70b4d4e14e6f8414cd79f3
Anjali-Thakurr <a7876110050t@gmail.com>
2026-04-14 12:23:14 +0530

    update c++

---
+++ c++codes/reverse-triangle-alphabet.cpp
@@ -0,0 +1,24 @@
+// 
+#include <iostream> 
+using namespace std; 
+
+int main()
+    {
+    char a,j=0  ;
+    
+    cout << "enter :";
+cin >> a ;
+char sum='A';
+    for(char i='A'; i<=a; i++){
+           
+      for( j='A'; j<=i ;j++){
+        
+       cout << sum << "\t";
+      sum--;
+      }
+       sum=j;
+      cout << endl ;
+    }
+      
+  return 0;
+  }
