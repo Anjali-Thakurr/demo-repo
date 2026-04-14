fbf55dcdf3f78e6e3c23d601d25f04326ea6f1d5
Anjali-Thakurr <a7876110050t@gmail.com>
2026-04-14 10:26:56 +0530

    new c++ code

---
+++ c++codes/reverse-triangle-patttern.cpp
@@ -0,0 +1,22 @@
+// reverse triangle pattern
+#include <iostream> 
+using namespace std; 
+
+int main()
+    {
+    int a  ;
+    
+    cout << "enter :";
+cin >> a ;
+    for(int i=0;i<a;i++){
+      int sum=i+1;
+      for(int j=0;j<i+1;j++){
+       cout << sum;
+      sum--;
+      }
+       
+      cout << endl ;
+    }
+      
+  return 0;
+  }
